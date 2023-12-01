1267458e2b44a7c28822e31d5d061874bc8b9b7e
tego <mohamed.elnemr@students.beaminstitute.org.com>
2023-12-02 00:39:37 +0100

    done unit test

--- src/Calculator.test.jsx
+++ src/Calculator.test.jsx
@@ -1,185 +1,246 @@
- import { describe, test, expect } from 'vitest';
- import {  render,screen,waitFor } from '@testing-library/react';
- 
- import userEvent from '@testing-library/user-event';
+import { describe, test, expect } from 'vitest';
+import { render, screen, waitFor } from '@testing-library/react';
+
+import userEvent from '@testing-library/user-event';
 import Calculator from './components/Calculator';
 
 
 describe('Calculator component', () => {
+
   test('renders minus button', () => {
     render(<Calculator />);
     const minusButton = screen.getByRole('button', { name: /-/i });
-    expect(minusButton).toBeVisible();
+    expect(minusButton).toBeInTheDocument();
   });
 
-test('renders plus button', () => {
-  render(<Calculator />);
-  let plusButton = screen.getByRole('button', {name: /\+/i })
+  test('renders plus button', () => {
+    render(<Calculator />);
+    const plusButton = screen.getByRole('button', { name: /\+/i });
     expect(plusButton).toBeInTheDocument();
-});
 
-test('renders button with label "5"', () => {
-  render(<Calculator />);
-  let Button5 = screen.getByRole('button', {name: /5/i })
+  });
+
+  test('renders button with label "5"', () => {
+    render(<Calculator />);
+    const Button5 = screen.getByRole('button', { name: /5/i })
     expect(Button5).toBeInTheDocument();
-});
+  });
 
-test('renders button with label "1"', () => {
-  render(<Calculator />);
-  let Button1 = screen.getByRole('button', {name: /1/i })
+  test('renders button with label "1"', () => {
+    render(<Calculator />);
+    const Button1 = screen.getByRole('button', { name: /1/i })
     expect(Button1).toBeInTheDocument();
-});
-test('renders button with label "9"', () => {
-  render(<Calculator />);
-  let Button9 = screen.getByRole('button', {name: /9/i })
+  });
+  test('renders button with label "9"', () => {
+    render(<Calculator />);
+    const Button9 = screen.getByRole('button', { name: /9/i })
     expect(Button9).toBeInTheDocument();
-});
+  });
 
-test('renders equal button', () => {
-  render(<Calculator />);
-  let equalButton = screen.getByRole('button', {name: /=/i })
+  test('renders equal button', () => {
+    render(<Calculator />);
+    const equalButton = screen.getByRole('button', { name: /=/i })
     expect(equalButton).toBeInTheDocument();
-});
+  });
 
-test('renders divide button', () => {
-  render(<Calculator />);
-  let divideButton = screen.getByRole('button', {name: /\//i })
+  test('renders divide button', () => {
+    render(<Calculator />);
+    const divideButton = screen.getByRole('button', { name: /\//i })
     expect(divideButton).toBeInTheDocument();
-});
+  });
 
-test('renders multiply button', () => {
-  render(<Calculator />);
-  let multiplyButton = screen.getByRole('button', {name: /\*/i })
+  test('renders multiply button', () => {
+    render(<Calculator />);
+    const multiplyButton = screen.getByRole('button', { name: /\*/i })
     expect(multiplyButton).toBeInTheDocument();
-});
+  });
 
-test('renders clear button', () => {
-  render(<Calculator />);
-  let ClearButton = screen.getByRole('button', {name: /C/i })
+  test('renders clear button', () => {
+    render(<Calculator />);
+    const ClearButton = screen.getByRole('button', { name: /C/i })
     expect(ClearButton).toHaveValue('');
-});
+  });
 
 
-test('renders the input on the screen', () => {
-  render(<Calculator />);
-  let input = screen.getByRole('textbox')
+  test('renders the input on the screen', () => {
+    render(<Calculator />);
+    let input = screen.getByRole('textbox')
     expect(input).toBeInTheDocument();
-
-
-});
-
-test('empty input', () => {
-  render(<Calculator/>)
-  const emptyInput = screen.getByRole('textbox')
-  expect (emptyInput).toHaveValue('');
-});
-
-
-  
-
-test('handles plus operation', async () => {
-  render(<Calculator />);
-  
-  const input = screen.getByRole('textbox');
-
-  await userEvent.click(screen.getByText('1'));
-  await userEvent.click(screen.getByText('+'));
-  await userEvent.click(screen.getByText('2'));
-  await userEvent.click(screen.getByText('+'));
-  await userEvent.click(screen.getByText('3'));
-  await userEvent.click(screen.getByText('='));
-
-  // Wait for the result to be displayed
-  await waitFor(() => {
-    expect(input).toHaveValue('6');
   });
-});
 
-
-test('handles diveded operation', async () => {
-  render(<Calculator />);
-  
-  const input = screen.getByRole('textbox');
-
-  await userEvent.click(screen.getByText('8'));
-  await userEvent.click(screen.getByText('/'));
-  await userEvent.click(screen.getByText('2'));
-  
-  await userEvent.click(screen.getByText('='));
-
-  // Wait for the result to be displayed
-  await waitFor(() => {
-    expect(input).toHaveValue('4');
+  test('empty input', () => {
+    render(<Calculator />)
+    const emptyInput = screen.getByRole('textbox')
+    expect(emptyInput).toHaveValue('');
   });
-});
 
-test('handles multiply operation', async () => {
-  render(<Calculator />);
-  
-  const input = screen.getByRole('textbox');
 
-  await userEvent.click(screen.getByText('8'));
-  await userEvent.click(screen.getByText('*'));
-  await userEvent.click(screen.getByText('2'));
-  
-  await userEvent.click(screen.getByText('='));
 
-  // Wait for the result to be displayed
-  await waitFor(() => {
-    expect(input).toHaveValue('16');
+
+  test('handles plus operation', async () => {
+    render(<Calculator />);
+
+
+    await userEvent.click(screen.getByText('1'));
+    await userEvent.click(screen.getByText('+'));
+    await userEvent.click(screen.getByText('2'));
+    await userEvent.click(screen.getByText('+'));
+    await userEvent.click(screen.getByText('3'));
+    await userEvent.click(screen.getByText('='));
+
+    await waitFor(() => {
+      expect(screen.getByRole('textbox')).toHaveValue('6');
+    });
+
   });
-});
 
-test('handles minus operation', async () => {
-  render(<Calculator />);
-  
-  const input = screen.getByRole('textbox');
 
-  await userEvent.click(screen.getByText('9'));
-  await userEvent.click(screen.getByText('-'));
-  await userEvent.click(screen.getByText('2'));
-  
-  await userEvent.click(screen.getByText('='));
 
-  // Wait for the result to be displayed
-  await waitFor(() => {
-    expect(input).toHaveValue('7');
+  test('handles diveded operation', async () => {
+    render(<Calculator />);
+
+
+    await userEvent.click(screen.getByText('8'));
+    await userEvent.click(screen.getByText('/'));
+    await userEvent.click(screen.getByText('2'));
+    await userEvent.click(screen.getByText('='));
+
+    await waitFor(() => {
+      expect(screen.getByRole('textbox')).toHaveValue('4');
+    });
   });
-});
 
-test('handles multiply operation with wrong result', async () => {
-  render(<Calculator />);
-  
-  const input = screen.getByRole('textbox');
+  test('handles multiply operation', async () => {
+    render(<Calculator />);
 
-  await userEvent.click(screen.getByText('8'));
-  await userEvent.click(screen.getByText('*'));
-  await userEvent.click(screen.getByText('2'));
-  await userEvent.click(screen.getByText('='));
+    await userEvent.click(screen.getByText('8'));
+    await userEvent.click(screen.getByText('*'));
+    await userEvent.click(screen.getByText('2'));
+    await userEvent.click(screen.getByText('='));
 
-  // Wait for the result to be displayed
-  await waitFor(() => {
-    expect(input).not.toHaveValue('15');
+    await waitFor(() => {
+      expect(screen.getByRole('textbox')).toHaveValue('16');
+    });
+
   });
-});
 
-test('handles clear result ', async () => {
-  render(<Calculator />);
-  
-  const input = screen.getByRole('textbox');
+  test('handles minus operation', async () => {
+    render(<Calculator />);
 
-  await userEvent.click(screen.getByText('8'));
-  await userEvent.click(screen.getByText('*'));
-  await userEvent.click(screen.getByText('2'));
-  await userEvent.click(screen.getByText('='));
-  await userEvent.click(screen.getByText('C'));
-  // Wait for the result to be displayed
-  await waitFor(() => {
-    expect(input).toHaveValue('');
+
+    await userEvent.click(screen.getByText('9'));
+    await userEvent.click(screen.getByText('-'));
+    await userEvent.click(screen.getByText('2'));
+    await userEvent.click(screen.getByText('='));
+
+    await waitFor(() => {
+      expect(screen.getByRole('textbox')).toHaveValue('7');
+    });
+
   });
-});
+
+  test('handles multiply operation with wrong result', async () => {
+    render(<Calculator />);
+
+    await userEvent.click(screen.getByText('8'));
+    await userEvent.click(screen.getByText('*'));
+    await userEvent.click(screen.getByText('2'));
+    await userEvent.click(screen.getByText('='));
+
+    await waitFor(() => {
+      expect(screen.getByRole('textbox')).not.toHaveValue('10');
+    });
+
+  });
+
+  test('handles clear result ', async () => {
+    render(<Calculator />);
+
+    await userEvent.click(screen.getByText('8'));
+    await userEvent.click(screen.getByText('*'));
+    await userEvent.click(screen.getByText('2'));
+    await userEvent.click(screen.getByText('='));
+    await userEvent.click(screen.getByText('C'));
+
+    await waitFor(() => {
+      expect(screen.getByRole('textbox')).toHaveValue('');
+    });
+
+  });
 
 
+  test('handles calculation error gracefully', async () => {
+    render(<Calculator />);
+
+    await userEvent.click(screen.getByText('0'));
+    await userEvent.click(screen.getByText('1'));
+    await userEvent.click(screen.getByText('/'));
+    await userEvent.click(screen.getByText('0'));
+    await userEvent.click(screen.getByText('='));
+
+    await waitFor(() => {
+      expect(screen.getByRole('textbox')).toHaveValue('Error');
+    });
+  });
+
+  test('handles percentage operation', async () => {
+    render(<Calculator />);
+  
+    await userEvent.click(screen.getByText('5'));
+    await userEvent.click(screen.getByText('%'));
+  
+    await waitFor(() => {
+      expect(screen.getByRole('textbox')).toHaveValue('5%');
+    });
+  });
+  
+  test('handles dot operation', async () => {
+    render(<Calculator />);
+  
+    await userEvent.click(screen.getByText('3'));
+    await userEvent.click(screen.getByText('.'));
+    await userEvent.click(screen.getByText('2'));
+  
+    await waitFor(() => {
+      expect(screen.getByRole('textbox')).toHaveValue('3.2');
+    });
+  });
+  
+  test('handles parentheses operation', async () => {
+    render(<Calculator />);
+  
+    await userEvent.click(screen.getByText('('));
+    await userEvent.click(screen.getByText('8'));
+    await userEvent.click(screen.getByText('+'));
+    await userEvent.click(screen.getByText('2'));
+    await userEvent.click(screen.getByText(')'));
+    await userEvent.click(screen.getByText('*'));
+    await userEvent.click(screen.getByText('3'));
+    await userEvent.click(screen.getByText('='));
+  
+    await waitFor(() => {
+      expect(screen.getByRole('textbox')).toHaveValue('30');
+    });
+  });
+  
+
+  test('handles unbalanced parentheses error', async () => {
+    render(<Calculator />);
+  
+    // Simulate input with unbalanced parentheses
+    await userEvent.click(screen.getByText('6'));
+    await userEvent.click(screen.getByText('*'));
+    await userEvent.click(screen.getByText('('));
+    await userEvent.click(screen.getByText(')'));
+    
+    userEvent.click(screen.getByText('='));
+  
+    // Check if the input displays the error message
+    await waitFor(() => {
+      expect(screen.getByRole('textbox')).toHaveValue('Error');
+    });
+  });
 
 
+  
 })
--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
- function Calculator() {
+function Calculator() {
   const [input, setInput] = useState('');
 
   const handleClick = (value) => {
@@ -13,43 +13,78 @@ import { useState } from 'react';
 
   const handleCalculate = () => {
     console.log('handleCalculate');
+  
     try {
-      const result = eval(input);
+      // Replace % with *0.01 for percentage calculation
+      let expression = input.replace(/%/g, '*0.01');
+      
+      // Handle parentheses for evaluation
+      const parenthesesBalanced = (str) => {
+        const stack = [];
+        for (let char of str) {
+          if (char === '(') stack.push('(');
+          else if (char === ')') {
+            if (stack.length === 0) return false;
+            stack.pop();
+          }
+        }
+        return stack.length === 0;
+      };
+
+      if (!parenthesesBalanced(expression)) {
+        setInput('Error');
+        return;
+      }
+
+// using eval to calculate the result and updates the input.
+      const result = eval(expression);
       setInput(result.toString());
     } catch (error) {
       setInput('Error');
     }
+
   };
 
   return (
     <div className="calculator">
-      <input type="text" value={input} readOnly/>
+      <input type="text" value={input} readOnly />
       <div className="buttons">
         <div className="row">
           <button onClick={() => handleClick('1')} className="operator" aria-label="1">1</button>
           <button onClick={() => handleClick('4')} className="operator" aria-label="4">4</button>
           <button onClick={() => handleClick('7')} className="operator" aria-label="7">7</button>
+          <button onClick={() => handleClick('/')} className="operator divide" aria-label="/">/</button>
           <button onClick={() => handleClick('+')} className="operator plus" aria-label="+">+</button>
+
+
         </div>
         <div className="row">
-          
+
           <button onClick={() => handleClick('2')} className="operator" aria-label="2">2</button>
           <button onClick={() => handleClick('5')} className="operator" aria-label="5">5</button>
           <button onClick={() => handleClick('8')} className="operator" aria-label="8">8</button>
           <button onClick={() => handleClick('-')} className="operator minus" aria-label="-">-</button>
+          <button onClick={() => handleClick('(')} className="operator left Parentheses" aria-label="(">(</button>
+
         </div>
         <div className="row">
-          
+
           <button onClick={() => handleClick('3')} className="operator" aria-label="3">3</button>
           <button onClick={() => handleClick('6')} className="operator" aria-label="6">6</button>
           <button onClick={() => handleClick('9')} className="operator" aria-label="9">9</button>
           <button onClick={() => handleClick('*')} className="operator multiply" aria-label="*">*</button>
+          <button onClick={() => handleClick(')')} className="operator right Parentheses" aria-label=")">)</button>
+
         </div>
         <div className="row">
           <button onClick={() => handleClick('0')} className="operator" aria-label="0">0</button>
           <button onClick={handleClear} className="clear" aria-label="C">C</button>
           <button onClick={handleCalculate} className="equal" aria-label="=">=</button>
-          <button onClick={() => handleClick('/')} className="operator divide" aria-label="/">/</button>
+          <button onClick={() => handleClick('.')} className="operator dot" aria-label=".">.</button>
+          <button onClick={() => handleClick('%')} className="operator percentage" aria-label="%">%</button>
+
+
+
         </div>
       </div>
     </div>
